525c43921db6b57fce531c5f0a49ac57feee963e
vlady <vladysha1021@gmail.com>
2025-04-15 12:54:41 -0400

    Add parallel column max processing

--- MatrixOperations/MatrixOperations.cpp
+++ MatrixOperations/MatrixOperations.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <ctime>
 #include <iomanip>
+#include <thread>
+#include <vector>
 #include "MatrixOperations.h"
 
 int* MatrixOperations::allocateMatrix(int size) {
@@ -18,6 +20,46 @@ void MatrixOperations::fillMatrix(int* matrix, int size) {
     for (int row = 0; row < size; row++) {
         for (int col = 0; col < size; col++) {
             matrix[row * size + col] = 1 + rand() % 10000;
+        }
+    }
+}
+
+void MatrixOperations::calculateMaxOfColumn(int* matrix, int size, int col) {
+    if (col >= size)
+        return;
+
+    int maxRow = col;
+    int maxVal = matrix[col * size + col];
+
+    for (int row = 0; row < size; row++) {
+        if (matrix[row * size + col] > maxVal) {
+            maxVal = matrix[row * size + col];
+            maxRow = row;
+        }
+    }
+
+    if (maxRow != col) {
+        std::swap(matrix[col * size + col], matrix[maxRow * size + col]);
+    }
+}
+
+void MatrixOperations::calculateMaxMultiThread(int* matrix, int size, int threadCount, std::atomic<int>* progressTracker) {
+    std::vector<std::thread> threads(threadCount);
+
+    for (int threadId = 0; threadId < threadCount; threadId++) {
+        threads[threadId] = std::thread(calculateMaxForColumns,matrix, size, threadId, threadCount, progressTracker);
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+}
+
+void MatrixOperations::calculateMaxForColumns(int* matrix, int size, int threadId, int threadNum, std::atomic<int>* progressTracker) {
+    for (int col = threadId; col < size; col += threadNum) {
+        calculateMaxOfColumn(matrix, size, col);
+        if (progressTracker) {
+            (*progressTracker)++;
         }
     }
 }
--- MatrixOperations/MatrixOperations.h
+++ MatrixOperations/MatrixOperations.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <atomic>
 
 class MatrixOperations {
 public:
@@ -6,4 +7,7 @@ public:
     static void freeMatrix(int* matrix);
     static void fillMatrix(int* matrix, int size);
     static void printMatrix(int* matrix, int size);
+    static void calculateMaxOfColumn(int* matrix, int size, int col);
+    static void calculateMaxMultiThread(int* matrix, int size, int threadCount, std::atomic<int>* progressTracker = nullptr);
+    static void calculateMaxForColumns(int* matrix, int size, int threadId, int threadNum, std::atomic<int>* progressTracker = nullptr);
 };
