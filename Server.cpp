c29a9df03d70b01a05b75a026c62da78a6b01f0d
vlady <vladysha1021@gmail.com>
2025-04-15 07:13:09 -0400

    Initial server socket setup

--- Server.cpp
+++ Server.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <winsock2.h>
+#pragma comment(lib,"ws2_32.lib")
 
+class Server {
+private:
+    SOCKET serverSocket;
+    int port = 8080;
+    bool serverRunning  = true;
+
+public:
+    bool initializeServer() {
+        WSADATA wsaData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+            std::cout << "WSAStartup failed" << std::endl;
+            return false;
+        }
+        serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (serverSocket == INVALID_SOCKET) {
+            std::cout << "Socket creation failed: " << WSAGetLastError() << std::endl;
+            WSACleanup();
+            return false;
+        }
+        sockaddr_in serverAddress;
+        serverAddress.sin_family = AF_INET;
+        serverAddress.sin_addr.s_addr = INADDR_ANY;
+        serverAddress.sin_port = htons(port);
+        if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
+            std::cout << "Socket binding failed: " << WSAGetLastError() << std::endl;
+            closesocket(serverSocket);
+            WSACleanup();
+            return false;
+        }
+        return true;
+    }
+};
